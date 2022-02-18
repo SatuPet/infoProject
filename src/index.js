9d33fc9348cc6d4f1a23cb5030e1aecec2df4b74
SatuPet <satupet@metropolia.fi>
2022-02-18 16:40:08 +0200

    background

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
-console.log('Hello console!');
+window.addEventListener('scroll',(event) =>{
+
+    let top = this.pageYOffset;
+    
+    const icons = document.querySelectorAll("[data-type='parallax']");
+    let icon, speed, yPosition;
+    
+    for (let i = 0; i < icons.length; i++) {
+        icon = icons[i];
+        speed = icon.getAttribute('data-speed');
+        let yPosition = -(pageYOffset * speed / 100);
+        icon.setAttribute('style', 'transform: translate3d(0px, ' + yPosition + 'px, 0px)');
+    
+    }
+    
+    });
