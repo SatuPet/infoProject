f0fa2cba7eb4744a6cb0176f2fbeccabc621c77a
j4zuu <jasu.honkanen2@gmail.com>
2022-02-19 20:58:35 +0200

    hook structure done and weather fetch works

---
+++ src/assets/jsModules/ApiConfig.js
@@ -0,0 +1,7 @@
+const ApiConfig = {
+  weatherApiUrl: 'https://api.openweathermap.org/data/2.5/weather?q=',
+  weatherApiKey: ',fi&appid=72f6f1c57db7f716dd2cd7a50e479d3f',
+  fazerApiUrl: 'https://www.foodandco.fi/modules/json/json/Index?costNumber=3208&language=',
+};
+
+export {ApiConfig};
