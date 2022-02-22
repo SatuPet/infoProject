fe1ff661c58bc9ccf18ec167de228070811a2e29
SatuPet <satupet@metropolia.fi>
2022-02-22 14:18:37 +0200

    .

--- src/index.js
+++ src/index.js
@@ -5,14 +5,14 @@ weather();
 // parallax Y postition moving
 window.addEventListener('scroll',(event) =>{
 
-    let top = this.pageYOffset;
+    let top = this.scrollY;
     const icons = document.querySelectorAll("[data-type='parallax']");
     let icon, speed, yPosition;
     
     for (let i = 0; i < icons.length; i++) {
         icon = icons[i];
         speed = icon.getAttribute('data-speed');
-        let yPosition = -(pageYOffset * speed / 100);
+        const yPosition = -(top * speed / 100);
         icon.setAttribute('style', 'transform: translate3d(0px, ' + yPosition + 'px, 0px)');
     
     }
