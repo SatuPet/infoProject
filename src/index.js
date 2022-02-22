839114138de3ca0e5124d320259df57ef17e0137
j4zuu <jasu.honkanen2@gmail.com>
2022-02-22 15:14:19 +0200

    Combined everything

--- src/index.js
+++ src/index.js
@@ -21,24 +21,17 @@ window.addEventListener('scroll', (event) => {
     getBusses(2132226);
   });
 //getBusses(2132207);
-
   window.addEventListener('scroll', (event) => {
-
-    let top = this.pageYOffset;
     const icons = document.querySelectorAll('[data-type=\'parallax\']');
-    let icon, speed, yPosition;
-
+    let icon, speed;
     for (let i = 0; i < icons.length; i++) {
       icon = icons[i];
       speed = icon.getAttribute('data-speed');
-      let yPosition = -(pageYOffset * speed / 100);
+      let yPosition = -(scrollY * speed / 100);
       icon.setAttribute('style',
         'transform: translate3d(0px, ' + yPosition + 'px, 0px)');
-
     }
-
   });
-
   //navbar collapse moving back when you touch somewere else
   const closeNavbar = () => {
     $('.navbar-collapse').collapse('hide');
