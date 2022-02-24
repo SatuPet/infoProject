a9e849d8c7ddf11623e658e7f606a0bdba704a27
SatuPet <satupet@metropolia.fi>
2022-02-24 11:49:34 +0200

    darksMode

--- src/index.js
+++ src/index.js
@@ -19,12 +19,68 @@ const closeNavbar = () => {
   bsCollapse.hide();
 };
 
-document.querySelector('body').addEventListener('click', closeNavbar);
+// document.querySelector('.background').addEventListener('click', closeNavbar);
 document.querySelector('.metroMeno').addEventListener('click', closeNavbar);
 
 
 
 
 
+const darkModeToggleButton = document.querySelector('#flexSwitchCheckDefault1');
+let darkModeStyleSheet;
+
+const changeDarkMode = () => {
+    const darkModeSetting = localStorage.getItem('darkModeSetting');
+    if (darkModeSetting === null) {
+        localStorage.setItem('darkModeSetting', 'whitemode');
+    } else {
+        
+        if (darkModeSetting == 'whitemode') {
+            // Creating the darkmode css style sheet and adding it to DOM
+            darkModeStyleSheet = document.createElement('link');
+            darkModeStyleSheet.rel = 'stylesheet';
+            darkModeStyleSheet.href = "assets/darkMode.css";
+            document.head.appendChild(darkModeStyleSheet);
+
+            // Saving preference to storage
+            localStorage.setItem('darkModeSetting', 'darkmode');
+        }
+        else {
+            if (darkModeStyleSheet != undefined) {
+                console.log(darkModeStyleSheet);
+                darkModeStyleSheet.remove();
+            }
+
+            // Saving preference to storage
+            localStorage.setItem('darkModeSetting', 'whitemode');
+        }
+
+    }
+
+};
+
+darkModeToggleButton.addEventListener('click',changeDarkMode);
+
+const setTheme = () => {
+  const darkModeSetting = localStorage.getItem('darkModeSetting');
+  if (darkModeSetting === null) {
+      localStorage.setItem('darkModeSetting', 'whitemode');
+  } else {
+      
+      if (darkModeSetting == 'darkmode') {
+          // Creating the darkmode css style sheet and adding it to DOM
+          darkModeStyleSheet = document.createElement('link');
+          darkModeStyleSheet.rel = 'stylesheet';
+          darkModeStyleSheet.href = "assets/darkMode.css";
+          document.head.appendChild(darkModeStyleSheet);
+          document.getElementById("flexSwitchCheckDefault1").checked = true;
+      }
+
+  }
+};
 
 
+const init = () => {
+  setTheme();
+};
+init();
