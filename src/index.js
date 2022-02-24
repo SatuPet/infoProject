a3b7e20ff5e2827f9debf15c0ead6ff80b82e49c
SatuPet <satupet@metropolia.fi>
2022-02-24 09:59:44 +0200

    navbar removing index.js

--- src/index.js
+++ src/index.js
@@ -1,26 +1,30 @@
-import {weather} from './assets/jsModules/test';
+import { weather } from './assets/jsModules/test';
 
 weather();
 
 // parallax Y postition moving
-window.addEventListener('scroll',(event) =>{
+window.addEventListener('scroll', (event) => {
 
-    let top = window.scrollY /2;
-    const background = document.querySelector("body");
-    background.style.backgroundPositionY = top+"px";
-    });
+  let top = window.scrollY / 2;
+  const background = document.querySelector("body");
+  background.style.backgroundPositionY = top + "px";
+});
 
-    //navbar collapse moving back when you touch somewere else
-    const closeNavbar = () => {
-        $('.navbar-collapse').collapse('hide');
-    };
+//navbar collapse moving back when you touch somewere else
+const menuToggle = document.getElementById('navbarToggleExternalContent');
+const bsCollapse = new bootstrap.Collapse(menuToggle, {toggle: false});
 
-    // document.querySelector('.background').addEventListener('click',closeNavbar);
-    // document.querySelector('.metroMeno').addEventListener('click',closeNavbar);
-    
-
+const closeNavbar = () => {
  
-     
+  bsCollapse.hide();
+};
+
+document.querySelector('body').addEventListener('click', closeNavbar);
+document.querySelector('.metroMeno').addEventListener('click', closeNavbar);
+
+
+
+
 
 
 
