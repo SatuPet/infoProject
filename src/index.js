b88b10242f873085a780116eddb65fae0170edc9
SatuPet <satupet@metropolia.fi>
2022-02-25 19:59:18 +0200

    busStops

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
-import {weather} from './assets/jsModules/weather';
-import {getBusses} from './assets/jsModules/hsl';
+import { weather } from './assets/jsModules/weather';
+import { getBusses } from './assets/jsModules/hsl';
 
 weather();
 
 // parallax Y postition moving
 window.addEventListener('scroll', (event) => {
+  // KARAMALMI
   document.getElementById('karanristiEast').addEventListener('click', () => {
     getBusses(2132208);
   });
@@ -20,25 +21,74 @@ window.addEventListener('scroll', (event) => {
   document.getElementById('karamalmensEast').addEventListener('click', () => {
     getBusses(2132226);
   });
-});
-//getBusses(2132207);
 
+    // ARABIA
+  document.getElementById('KaironkatuNorth').addEventListener('click', () => {
+    getBusses(1230102);
+  });
+  document.getElementById('KaironkatuSouth').addEventListener('click', () => {
+    getBusses(1230101);
+  });
+  document.getElementById('Arabianranta').addEventListener('click', () => {
+    getBusses(1232102);
+  });
+  document.getElementById('ArabianrantaSpora').addEventListener('click', () => {
+    getBusses(1230407);
+  });
+
+  // MYLLYPURO
+  document.getElementById('LiikuntamyllyWest').addEventListener('click', () => {
+    getBusses(1454111);
+  });
+  document.getElementById('LiikuntamyllyWEast').addEventListener('click', () => {
+    getBusses(1454112);
+  });
+  document.getElementById('MyllypuronTervAs').addEventListener('click', () => {
+    getBusses(1454138);
+  });
+  document.getElementById('MyllypuronMetroasema').addEventListener('click', () => {
+    getBusses(1454601), getBusses(1454602);;
+  });
+  document.getElementById('MyllypuroMEast').addEventListener('click', () => {
+    getBusses(1454140);
+  });
+  document.getElementById('MyllypuroMWest').addEventListener('click', () => {
+    getBusses(1454141);
+  });
+
+  // MYYRMÄKI
+  document.getElementById('MyyrmakiJunaAsema').addEventListener('click', () => {
+    getBusses(4150551), getBusses(4150501);
+  });
+  document.getElementById('LeiritieEast').addEventListener('click', () => {
+    getBusses(4150201);
+  });
+  document.getElementById('LeiritieWest').addEventListener('click', () => {
+    getBusses(4150296);
+  });
+  document.getElementById('Raappavuorentie').addEventListener('click', () => {
+    getBusses(4150228);
+  });
+  document.getElementById('Honkasuo').addEventListener('click', () => {
+    getBusses(4150269);
+  });
+});
 
 
 // "parallax" Y postition moving
-window.addEventListener('scroll',(event) =>{
+window.addEventListener('scroll', (event) => {
 
-  let top = window.scrollY /2;
+  let top = window.scrollY / 2;
   const background = document.querySelector("body");
-  background.style.backgroundPositionY = top+"px";
-  });
-  
+  background.style.backgroundPositionY = top + "px";
+});
+
 //navbar collapse moving back when you touch somewere else
 const menuToggle = document.getElementById('navbarToggleExternalContent');
-const bsCollapse = new bootstrap.Collapse(menuToggle, {toggle: false});
+const bsCollapse = new bootstrap.Collapse(menuToggle, { toggle: false });
 
 const closeNavbar = () => {
- 
+
   bsCollapse.hide();
 };
 
@@ -51,51 +101,51 @@ const darkModeToggleButton = document.querySelector('#flexSwitchCheckDefault1');
 let darkModeStyleSheet;
 
 const changeDarkMode = () => {
-    const darkModeSetting = localStorage.getItem('darkModeSetting');
-    if (darkModeSetting === null) {
-        localStorage.setItem('darkModeSetting', 'whitemode');
-    } else {
-        
-        if (darkModeSetting == 'whitemode') {
-            // Creating the darkmode css style sheet and adding it to DOM
-            darkModeStyleSheet = document.createElement('link');
-            darkModeStyleSheet.rel = 'stylesheet';
-            darkModeStyleSheet.href = "assets/cssModules/darkMode.css";
-            document.head.appendChild(darkModeStyleSheet);
+  const darkModeSetting = localStorage.getItem('darkModeSetting');
+  if (darkModeSetting === null) {
+    localStorage.setItem('darkModeSetting', 'whitemode');
+  } else {
 
-            // Saving preference to storage
-            localStorage.setItem('darkModeSetting', 'darkmode');
-        }
-        else {
-            if (darkModeStyleSheet != undefined) {
-                console.log(darkModeStyleSheet);
-                darkModeStyleSheet.remove();
-            }
+    if (darkModeSetting == 'whitemode') {
+      // Creating the darkmode css style sheet and adding it to DOM
+      darkModeStyleSheet = document.createElement('link');
+      darkModeStyleSheet.rel = 'stylesheet';
+      darkModeStyleSheet.href = "assets/cssModules/darkMode.css";
+      document.head.appendChild(darkModeStyleSheet);
 
-            // Saving preference to storage
-            localStorage.setItem('darkModeSetting', 'whitemode');
-        }
-
+      // Saving preference to storage
+      localStorage.setItem('darkModeSetting', 'darkmode');
     }
+    else {
+      if (darkModeStyleSheet != undefined) {
+        console.log(darkModeStyleSheet);
+        darkModeStyleSheet.remove();
+      }
+
+      // Saving preference to storage
+      localStorage.setItem('darkModeSetting', 'whitemode');
+    }
+
+  }
 
 };
 
-darkModeToggleButton.addEventListener('click',changeDarkMode);
+darkModeToggleButton.addEventListener('click', changeDarkMode);
 
 const setTheme = () => {
   const darkModeSetting = localStorage.getItem('darkModeSetting');
   if (darkModeSetting === null) {
-      localStorage.setItem('darkModeSetting', 'whitemode');
+    localStorage.setItem('darkModeSetting', 'whitemode');
   } else {
-      
-      if (darkModeSetting == 'darkmode') {
-          // Creating the darkmode css style sheet and adding it to DOM
-          darkModeStyleSheet = document.createElement('link');
-          darkModeStyleSheet.rel = 'stylesheet';
-          darkModeStyleSheet.href = "assets/cssModules/darkMode.css";
-          document.head.appendChild(darkModeStyleSheet);
-          document.getElementById("flexSwitchCheckDefault1").checked = true;
-      }
+
+    if (darkModeSetting == 'darkmode') {
+      // Creating the darkmode css style sheet and adding it to DOM
+      darkModeStyleSheet = document.createElement('link');
+      darkModeStyleSheet.rel = 'stylesheet';
+      darkModeStyleSheet.href = "assets/cssModules/darkMode.css";
+      document.head.appendChild(darkModeStyleSheet);
+      document.getElementById("flexSwitchCheckDefault1").checked = true;
+    }
 
   }
 };
