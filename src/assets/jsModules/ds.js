b9c9d03ce6d06af080fdfb2f48b31bc307991ce8
Mikael Gustafsson <mikaelgustafsson@Mikaels-MacBook-Pro.local>
2022-03-08 11:13:09 +0200

    ds menu list fix

--- src/assets/jsModules/ds.js
+++ src/assets/jsModules/ds.js
@@ -3,6 +3,9 @@ import {weather} from './weather';
 import {getMenus, selectedCampus} from './lunchMenu';
 
 let localCampus = selectedCampus;
+const dsMenuList = document.getElementById('ds-menu-list');
+const restaurantOpen = document.querySelector('#text-open');
+const menuPrices = document.querySelector('#text-prices');
 
 console.log('hello from ds.js');
 const timebox = document.getElementById('date-time');
@@ -47,6 +50,9 @@ const hslTimer = (campusLat, campusLon, campusRad) => {
 hslTimer(60.2241077, 24.7565312, 600); //default Karamalmi
 
 document.getElementById('arabia-campus').addEventListener('click', () => {
+  dsMenuList.innerHTML = '';
+  menuPrices.innerHTML = 'Hinnat: 2.70€ / 8.00€';
+  restaurantOpen.innerHTML = 'Avoinna: 10.30-13.30';
   getMenus('arabia');
   localCampus = 'arabia';
   console.log('name clicked');
@@ -55,6 +61,9 @@ document.getElementById('arabia-campus').addEventListener('click', () => {
   weather(60.2094084, 24.9809358);
 });
 document.getElementById('karamalmi-campus').addEventListener('click', () => {
+  dsMenuList.innerHTML = '';
+  menuPrices.innerHTML = 'Hinnat: 1.90€ / 2.70€ / 5.71€ (Opiskelijat)';
+  restaurantOpen.innerHTML = 'Avoinna: 11.00-13.15';
   getMenus('karamalmi');
   localCampus = 'karamalmi';
   console.log('name clicked');
@@ -63,6 +72,9 @@ document.getElementById('karamalmi-campus').addEventListener('click', () => {
   weather(60.2241077, 24.7565312);
 });
 document.getElementById('myllypuro-campus').addEventListener('click', () => {
+  dsMenuList.innerHTML = '';
+  menuPrices.innerHTML = 'Hinnat: 2.70€ / 5.50€ / 6.70€';
+  restaurantOpen.innerHTML = 'Avoinna: 10.30-14.00';
   getMenus('myllypuro');
   localCampus = 'myllypuro';
   console.log('name clicked');
@@ -71,6 +83,9 @@ document.getElementById('myllypuro-campus').addEventListener('click', () => {
   weather(60.2234938, 25.0757339);
 });
 document.getElementById('myyrmaki-campus').addEventListener('click', () => {
+  dsMenuList.innerHTML = '';
+  menuPrices.innerHTML = 'Hinnat: 2.70€ / 5.50€ / 6.70€';
+  restaurantOpen.innerHTML = 'Avoinna: 10.30-14.00';
   getMenus('myyrmaki');
   localCampus = 'myyrmaki';
   console.log('name clicked');
